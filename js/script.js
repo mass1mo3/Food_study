e95318fdc4b186513d4bb6987a81b3f0c91ba61d
Maksym <makushynskyi.m@gmail.com>
2022-11-14 20:02:13 +0200

    server

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
   //Tabs
@@ -229,4 +229,48 @@ window.addEventListener('DOMContentLoaded', () => {
               
 
             ).render();
+
+            //Forms
+
+            const forms = document.querySelectorAll('form');
+
+            const message = {
+              loading: "Загрузка",
+              success: "Спасибо! Скоро мы с Вами свяжемся",
+              failure: "Что-то пошло не так..."   
+            };
+
+            forms.forEach(item => {
+              postData(item);
+            });
+            
+            function postData(form) {
+              form.addEventListener('submit', (e) => {
+                e.preventDefault();
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest(); 
+                request.open('POST', 'server.php');
+                
+                request.setRequestHeader('Content-type', 'multipart/form-data');
+                const formData = new FormData(); 
+                
+                request.send(formData);
+
+                request.addEventListener('load', () => {
+                  if(request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+
+                  } else {
+                    statusMessage.textContent = message.failure;
+                  }
+                });
+              });
+            }
+
+
 });
