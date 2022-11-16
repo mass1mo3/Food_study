b069f1989f321154c6af08b8685463185b07ab6e
Massimo <makushisnkyi.m@gmail.com>
2022-11-16 18:25:59 +0200

    add fetch

--- js/script.js
+++ js/script.js
@@ -255,30 +255,34 @@ window.addEventListener('DOMContentLoaded', () => {
                 
                 form.insertAdjacentElement('afterend', statusMessage);
 
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
-                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+           
+             
                 const formData = new FormData(form);
 
               const object = {};
               formData.forEach(function(value, key){
                 object[key] = value;
             });
-                const json = JSON.stringify(object);
+                
 
-              request.send(json);
-
-              request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+                fetch('server.php', {
+                  method: 'POST',
+                  headers: {
+                    'Content-type': 'application/json'
+                  }, 
+                  body: JSON.stringify(object)
+                 }).then(data=> data.text())
+                 .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
-        });
+                 }).catch(() => {
+                  showThanksModal(message.failure);
+                 }).finally(() => {
+                  form.reset();
+                 })
+
+           });
     }
 
         function showThanksModal(message) {
@@ -303,4 +307,5 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
 });
